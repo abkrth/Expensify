f0b0c84a001eb5e52c64b59024f7bba231491498
Abhishek <abkrth1@gmail.com>
2021-04-14 03:37:49 +0530

    Add expense summary to dashboard

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -3,17 +3,18 @@ import moment from 'moment';
 // Get visible expenses
 
 export default (expenses, { text, sortBy, startDate, endDate }) => {
-    return expenses.filter((expense) => {
-        const createAtMoment = moment(expense.createdAt);
-        const startDateMatch = startDate ? startDate.isSameOrBefore(createAtMoment, 'day') : true;
-        const endDateMatch = endDate ? endDate.isSameOrAfter(createAtMoment, 'day') : true;
-        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
-        return startDateMatch && endDateMatch && textMatch;
-    }).sort((a, b) => {
-        if (sortBy === 'date')  {
-            return a.createdAt < b.createdAt ? 1 : -1;
-        } else if(sortBy === 'amount') {
-            return a.amount < b.amount ? 1 : -1;
-        }
-    });
+  return expenses.filter((expense) => {
+    const createdAtMoment = moment(expense.createdAt);
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
 };
