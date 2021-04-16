3a6828ad5f2a0c0ea8d6874622cbba1858cad269
Abhishek <abkrth1@gmail.com>
2021-04-16 08:37:31 +0530

    Add startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,6 +4,7 @@ import {
   startAddExpense, 
   addExpense, 
   editExpense, 
+  startEditExpense,
   removeExpense, 
   startRemoveExpense, 
   setExpenses, 
@@ -56,6 +57,26 @@ test('should setup edit expense action object', () => {
     }
   });
 });
+
+test('Should edit expense from firebase', (done) => {
+  const store = createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount : 6699 };
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    });
+
+    return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount);
+    done();
+  });
+});
+
 
 test('should setup add expense action object with provided values', () => {
 
